bf9733e4cde310c6ba4af35821cb95fc5ea652f0
Eric Brown <90017825+ericbrown8787@users.noreply.github.com>
2023-09-07 12:43:56 -0400

    Add tree movement

---
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float leftAndRightEdge = 10f;
+    public float chanceToChangeDirections = 0.1f;
+    public float secondsBetweenAppleDrops = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 pos = transform.position;
+
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        }
+
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 pos = transform.position;
+
+
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;
+        }
+    }
+}
