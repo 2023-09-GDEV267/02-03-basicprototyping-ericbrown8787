e0ba9c81d37bf3271ea1b7d1caab07a13f7a7812
Eric Brown <90017825+ericbrown8787@users.noreply.github.com>
2023-09-09 22:05:36 -0400

    Destroy basket upon dropping apple

--- Assets/01-Apple Picker/Scripts/ApplePicker.cs
+++ Assets/01-Apple Picker/Scripts/ApplePicker.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq; // Seems to be required to invoke List.Count
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -9,24 +11,22 @@ public class ApplePicker : MonoBehaviour
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i=0; i < numBaskets; i++)
+        basketList = new List<GameObject>();
+        for (int i = 0; i < numBaskets; i++)
         {
             GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    }    
+    
 
     public void AppleDestroyed() 
     {
@@ -36,6 +36,15 @@ public class ApplePicker : MonoBehaviour
         {
             Destroy(tGO);
         }
+
+        // Destroy one of the baskets
+        // Get the index of the last Basket in basketList
+        int basketIndex = basketList.Count() - 1;
+        // Get a reference to that Basket GameObject
+        GameObject tBasketGO = basketList[basketIndex];
+        // Remove the Basket from the list and destroy the GameObject
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
 
     }
 }
