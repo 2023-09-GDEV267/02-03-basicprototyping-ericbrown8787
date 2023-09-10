9bbcbd4596bfcd701f8528e8b9775389e6c755a3
Eric Brown <90017825+ericbrown8787@users.noreply.github.com>
2023-09-09 22:36:13 -0400

    Implement storing high score data in PlayerPrefs

--- Assets/01-Apple Picker/Scripts/HighScore.cs
+++ Assets/01-Apple Picker/Scripts/HighScore.cs
@@ -7,10 +7,26 @@ public class HighScore : MonoBehaviour
 {
     public static int score = 1000;
 
+    void Awake()
+    {
+        // If the PlayerPrefs HighScore already exists, read it
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            score = PlayerPrefs.GetInt("HighScore");
+        }
+
+        PlayerPrefs.SetInt("HighScore", score);
+    }
     // Update is called once per frame
     void Update()
     {
         TextMeshProUGUI gt = GetComponent<TextMeshProUGUI>();
         gt.text = $"High Score: {score}";
+
+        // Update the PlayerPrefs score if necessary
+        if (score > PlayerPrefs.GetInt("HighScore"))
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+        }
     }
 }
