ebc348f1d00b2ffeb9f1fc5cf4b4ddb566ed5b92
Eric Brown <90017825+ericbrown8787@users.noreply.github.com>
2023-09-11 13:21:17 -0400

    Complete exercise 2

--- Assets/01-Apple Picker/Scripts/Exercises/Exercises.cs
+++ Assets/01-Apple Picker/Scripts/Exercises/Exercises.cs
@@ -9,12 +9,21 @@ public class Exercises : MonoBehaviour
     void Start()
     {
         // Exercise 1
-        for (int i = 0; i < 10; i++)
+/*        for (int i = 0; i < 10; i++)
         {
             Instantiate(cubePrefab, new Vector3(0f, 0f, (float)i),Quaternion.identity);
         }
-
+*/
         //Exercise 2
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j< 8; j++)
+            {
+                Instantiate(cubePrefab, new Vector3((float)i, 0f, (float)j), Quaternion.identity);
+            }
+            
+        }
+
     }
 
     // Update is called once per frame
