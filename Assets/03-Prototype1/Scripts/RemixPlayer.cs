363b6c5b86c4ec9218de2fd03be03a2b46cf490c
Eric Brown <90017825+ericbrown8787@users.noreply.github.com>
2023-09-23 19:16:22 -0400

    Add player movement controls

--- Assets/03-Prototype1/Scripts/RemixPlayer.cs
+++ Assets/03-Prototype1/Scripts/RemixPlayer.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class RemixPlayer : MonoBehaviour
 {
+    public float speed = 10;
+    public float jumpForce = 60;
+
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+    }
+    void OnMove(InputValue movementValue)
+    {
+        Debug.Log("move");
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnJump()
     {
+        if (rb.velocity.y != 0 || !Physics.Raycast(transform.position, Vector3.down, 1.3f)) return;
+            Debug.Log("Jump");
+            movementY = jumpForce; movementY = jumpForce;
         
+
+
     }
+
+    void OnFire()
+    {
+        Debug.Log("fire");
+    }
+
+    void FixedUpdate()
+    {
+
+        Vector3 movement = new Vector3(movementX, movementY, 0.0f);
+        rb.AddForce(movement * speed);
+        if (movementY != 0) movementY = 0;
+        /*Debug.Log(movementX * speed);*/
+    }
+
 }
+
