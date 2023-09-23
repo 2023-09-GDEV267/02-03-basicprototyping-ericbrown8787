5f9a6ae6682f163c6b1223b07c5d4b365a7b17d5
Eric Brown <90017825+ericbrown8787@users.noreply.github.com>
2023-09-22 22:35:07 -0400

    Add slingshot rotation, aiming and firing with mouse

--- Assets/03-Prototype1/Scripts/HandheldSlingshot.cs
+++ Assets/03-Prototype1/Scripts/HandheldSlingshot.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
 
 public class HandheldSlingshot : MonoBehaviour
 {
+    public GameObject slingshotProjectilePrefab;
+    public float launchForce;
+    public Transform launchPoint;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,21 @@ public class HandheldSlingshot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 slingshotPosition = transform.position;
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePosition - slingshotPosition;
+        transform.right = direction;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            FireProjectile();
+        }
     }
+
+    void FireProjectile()
+    {
+        GameObject projectile = Instantiate(slingshotProjectilePrefab, launchPoint.position, launchPoint.rotation);
+        projectile.GetComponent<Rigidbody>().velocity=transform.right*launchForce;
+    }
+
 }
