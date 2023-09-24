4e5b2bdbe3497432d4c08896b1d0635bcc5cad86
Eric Brown <90017825+ericbrown8787@users.noreply.github.com>
2023-09-24 11:08:06 -0400

    Refactor and adjust player movement control feel

--- Assets/03-Prototype1/Scripts/RemixPlayer.cs
+++ Assets/03-Prototype1/Scripts/RemixPlayer.cs
@@ -5,12 +5,16 @@ using UnityEngine.InputSystem;
 
 public class RemixPlayer : MonoBehaviour
 {
-    public float speed = 10;
-    public float jumpForce = 60;
+    public float horizontal;
+    public float speed = 13f;
+    public float jumpForce = 25f;
+
+    private bool orientation = true;
 
     private Rigidbody rb;
     private float movementX;
     private float movementY;
+    private bool isJumping;
 
     // Start is called before the first frame update
     void Start()
@@ -19,19 +23,14 @@ public class RemixPlayer : MonoBehaviour
     }
     void OnMove(InputValue movementValue)
     {
-        Debug.Log("move");
         Vector2 movementVector = movementValue.Get<Vector2>();
         movementX = movementVector.x;
     }
 
-    void OnJump()
+    void OnJump(InputValue jumpValue)
     {
-        if (!Physics.Raycast(transform.position, Vector3.down, 1.5f)) return;
-            Debug.Log("Jump");
-            movementY = jumpForce; movementY = jumpForce;
-        
-
-
+        if (!IsGrounded()) return;
+        isJumping = true;
     }
 
     void OnFire()
@@ -39,13 +38,37 @@ public class RemixPlayer : MonoBehaviour
         Debug.Log("fire");
     }
 
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, 1.5f);
+    }
     void FixedUpdate()
     {
+        float xVelocity = speed;       
+        
+            if (movementX > 0)
+            {
+                rb.velocity = new Vector3(xVelocity, rb.velocity.y, 0.0f);
+            }
+            else if (movementX < 0)
+            {
+                rb.velocity = new Vector3(-xVelocity, rb.velocity.y, 0.0f);
+            }
+            if (movementY != 0) movementY = 0;
 
-        Vector3 movement = new Vector3(movementX, movementY, 0.0f);
+    
+
+        Vector3 movement = new Vector3(0.0f, movementY, 0.0f);
         rb.AddForce(movement * speed);
-        if (movementY != 0) movementY = 0;
-        /*Debug.Log(movementX * speed);*/
+ 
+
+
+        if (isJumping)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isJumping = false;
+        }
     }
 
 }
